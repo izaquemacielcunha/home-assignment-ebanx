0d52a1c917d708157033d0101549d708653e5ebd
izaquemacielcunha <izaquemaciel@hotmail.com>
2024-10-30 12:34:44 -0300

    initial commit

---
+++ src/main/java/com/ebanx/homeassignmentebanx/HomeAssignmentEbanxApplication.java
@@ -0,0 +1,13 @@
+package com.ebanx.homeassignmentebanx;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HomeAssignmentEbanxApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HomeAssignmentEbanxApplication.class, args);
+	}
+
+}
