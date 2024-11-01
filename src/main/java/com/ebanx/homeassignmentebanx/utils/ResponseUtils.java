87608baba9cd2019f26e64df419064b943333588
izaquemacielcunha <izaquemaciel@hotmail.com>
2024-11-01 00:37:49 -0300

    add class ResponseUtils for custom json response

---
+++ src/main/java/com/ebanx/homeassignmentebanx/utils/ResponseUtils.java
@@ -0,0 +1,40 @@
+package com.ebanx.homeassignmentebanx.utils;
+
+import java.util.LinkedHashMap;
+import java.util.Map;
+
+import com.ebanx.homeassignmentebanx.entity.Account;
+
+public class ResponseUtils {
+
+	public static Map<String, Object> buildDepositReponse(Account account) {
+		Map<String, Object> response = new LinkedHashMap<>();
+		Map<String, Object> accountMap = buildAccountMap(account);
+		response.put("destination", accountMap);
+		return response;
+	}
+
+	public static Map<String, Object> buildWithdrawReponse(Account account) {
+		Map<String, Object> response = new LinkedHashMap<>();
+		Map<String, Object> accountMap = buildAccountMap(account);
+		response.put("origin", accountMap);
+		return response;
+	}
+
+	public static Map<String, Object> buildTransferReponse(Account accountOrigin, Account accountDestination) {
+		Map<String, Object> response = new LinkedHashMap<>();
+		Map<String, Object> accountMapOrigin = buildAccountMap(accountOrigin);
+		response.put("origin", accountMapOrigin);
+		Map<String, Object> accountMapDestination = buildAccountMap(accountDestination);
+		response.put("destination", accountMapDestination);
+		return response;
+	}
+
+	private static Map<String, Object> buildAccountMap(Account account) {
+		Map<String, Object> accountMap = new LinkedHashMap<>();
+		accountMap.put("id", account.getId());
+		accountMap.put("balance", account.getBalance());
+		return accountMap;
+	}
+
+}// end of class
