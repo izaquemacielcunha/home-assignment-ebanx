13fb68838abeb6da1f7d0475c22fb622d9c15f45
izaquemacielcunha <izaquemaciel@hotmail.com>
2024-11-01 13:27:19 -0300

    add custom exceptions and exceptionhandler

---
+++ src/main/java/com/ebanx/homeassignmentebanx/exception/AccountNotFoundException.java
@@ -0,0 +1,5 @@
+package com.ebanx.homeassignmentebanx.exception;
+
+public class AccountNotFoundException extends RuntimeException{
+	private static final long serialVersionUID = 1L;
+}// end of class
